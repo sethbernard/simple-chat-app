d46f6c5a558b0713bed29eee72162033e682e9c1
Seth Bernard <sethbernard7@gmail.com>
2019-03-27 16:54:25 -0400

    Connect socket.io to client and server

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 
 app.use(express.static('client'));
 
-const server = app.listen(3000, function() {
+server.listen(3000, () => {
   console.log('listening on port 3000....');
+});
+
+io.on('connection', socket => {
+  console.log('a user connected');
 });
