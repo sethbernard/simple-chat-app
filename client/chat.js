dc0bd838253a24090c190f91c7afec5997b66038
Seth Bernard <sethbernard7@gmail.com>
2019-03-28 16:35:25 -0400

    Make messages send to the chat window on click

--- client/chat.js
+++ client/chat.js
@@ -1,1 +1,14 @@
 const socket = io.connect('http://localhost:3000');
+
+const input = document.getElementById('message-input');
+const button = document.getElementById('send-button');
+let output = document.getElementById('message-output');
+
+const sendText = () => {
+  socket.emit('chat', input.value);
+  output.innerHTML += '<p>' + input.value + '</p>';
+  input.value = '';
+  return false;
+};
+
+button.addEventListener('click', sendText);
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
-
-app.use(express.static('client'));
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 server.listen(3000, () => {
   console.log('listening on port 3000....');
 });
 
+app.use(express.static('client'));
+
 io.on('connection', socket => {
-  console.log('a user connected');
+  console.log('A user connected with the id: ', socket.id);
+  socket.on('chat', msg => {
+    console.log('message: ' + msg);
+  });
 });
